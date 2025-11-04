6e30a869b314cf048c4ffb943f2ad700eea935fa
JensPatomella <jens.patomella@skola.taby.se>
2025-11-04 13:30:40 +0100

    funkar inte just nu:(

--- test.py
+++ test.py
@@ -8,24 +8,26 @@ def data():
 
 def predicted(year):
     previous = data()
-    years = []
-    temps = []
-    c = 0
-    for i in previous:
-        if i[0].endswith("01-01"):
-            temp = float(i[1])
-            year_val = 0 + c
-            c += 1
-            years.append(year_val)
-            temps.append(temp)
-            print(np.where(previous == i[0]))
-            #previous = np.delete(previous, (idexet av "i" i "previous") , 0) för att det ska gå fortare desto fler datum som redan är klara
-
-    X = np.array(years).reshape(-1, 1)
-    y = np.array(temps)
-    model = LinearRegression()
-    model.fit(X, y)
-    prediction = model.predict([[year]])[0]
+    prediction = []
+    for k in range(1,13):
+        for l in range(1,32):
+            temps = []
+            years = []
+            c = 0
+            for i in previous:
+                if i[0].endswith("{k}-{l}"):
+                    temp = float(i[1])
+                    year_val = 0 + c
+                    c += 1
+                    years.append(year_val)
+                    temps.append(temp)
+                    index = np.where(previous == i[0])
+                    previous = np.delete(previous, index[0] , 0)
+            X = np.array(years).reshape(-1, 1)
+            y = np.array(temps)
+            model = LinearRegression()
+            model.fit(X, y)
+            prediction.append(model.predict([[year]])[0])
     return prediction
 
 print(predicted(28))
